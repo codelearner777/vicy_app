f057715f1f3457872fcf88b359929ee226c12d3f
mulyantobudi26@yahoo.com <mulyantobudi26@yahoo.com>
2016-01-25 21:41:40 +0700

    Finish static pages without css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	# Return the full title on a per-page basis.
+	def full_title(page_title = '')
+		base_title_1 = "VicyApp"
+		base_title_2 = "Try Building My Own Website"
+
+		if page_title.empty?
+			base_title_1
+		else
+			base_title_1 + " - " + page_title + " | " + base_title_2
+		end
+	end
+
+
+
 end
